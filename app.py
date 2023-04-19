6d0d58c5c032ae5e76f24d9fa58362acbd78c5aa
actuallysoham <soham.de2001@gmail.com>
2023-04-19 23:46:12 +0530

    next day

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def load_data(filename):
 
 
 #today = date.today()
-today = datetime.strptime('2023-04-19',"%Y-%m-%d").date()
+today = datetime.strptime('2023-04-20',"%Y-%m-%d").date()
 
 data_1 = load_data('day1')
 data_2 = load_data('day2')
@@ -100,8 +100,8 @@ if username_input in select_users:
 
 			st.markdown(html_str, unsafe_allow_html=True)
 			st.write("---")
-			st.text("Please fill the survey to complete today's task:")
-			st.markdown(f'''<a href='https://forms.gle/HBHD2mw9xZm1Hk1C8'>Link to Survey (Day 1)</a>''',unsafe_allow_html=True)
+			st.write("Please fill the survey to complete today's task:")
+			st.markdown(f'''<a href='https://forms.gle/HBHD2mw9xZm1Hk1C8'>Link to Survey (#1)</a>''',unsafe_allow_html=True)
 
 		else:
 			st.write("This page will be active on and after 2023-04-18")
@@ -150,7 +150,7 @@ if username_input in select_users:
 			st.markdown(html_str, unsafe_allow_html=True)
 			st.write("---")
 			st.write("Please fill the survey to complete today's task:")
-			st.markdown(f'''<a href='https://forms.gle/qDeb1Y6Mx7QS9UHn8'>Link to Survey (Day 3)</a>''',unsafe_allow_html=True)
+			st.markdown(f'''<a href='https://forms.gle/qDeb1Y6Mx7QS9UHn8'>Link to Survey (#2)</a>''',unsafe_allow_html=True)
 
 		else:
 			st.write("This page will be active on and after 2023-04-20")
